cf26ce041364e81e8083f44b346dd3f6eea7360a
Claude Lehmann <e.dualc@hotmail.com>
2021-10-05 20:33:06 +0200

    initial rotating pictures scene

--- unity/image_generation/Assets/TakeCameraPicture.cs
+++ unity/image_generation/Assets/TakeCameraPicture.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class TakeCameraPicture : MonoBehaviour
 {
-    public GameObject gameObject;
+    public GameObject prefab;
     public GameObject camera;
-    public float turnAmount = 0.1f;
+    public float turnAmount = 10.0f;
+    public int imgNumber = 0;
+    public int frameNumber = 0;
 
-    // Update is called once per frame
-    void Update()
+    private GameObject gameObject = null;
+
+    void Start()
     {
-        gameObject.transform.Rotate(turnAmount, turnAmount, turnAmount, Space.Self);
+        Screen.SetResolution(200, 200, false);
+        gameObject = GameObject.Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.Euler(45.0f, 60.0f, 45.0f));
+    }
+    void FixedUpdate()
+    {
+        Screen.SetResolution(200, 200, false);
+        
+        if (gameObject != null) {
+            float scaleX = Random.Range(0.0f, 2.0f);
+            float scaleY = Random.Range(0.0f, 2.0f);
+            float scaleZ = Random.Range(0.0f, 8.0f);
 
-        //Quaternion target = Quaternion.Euler(gameObject.transform.rotation.x + 1, 0, 0);
-        //gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, target, Time.deltaTime);
+            gameObject.transform.Rotate(scaleX*turnAmount, scaleY*turnAmount, scaleZ*turnAmount, Space.Self);
+
+            if ((imgNumber < 16) && (frameNumber % 50 == 0)) {
+                string filename = "cube_" + imgNumber.ToString() + ".png";
+                ScreenCapture.CaptureScreenshot(filename);
+                imgNumber++;
+            }
+
+            frameNumber++;
+        }
     }
 }
