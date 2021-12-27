5209358d75e90038a5ea4024e1c7d1eda0371126
Claude Lehmann <e.dualc@hotmail.com>
2021-12-27 17:12:27 +0100

    Improve Image Generation

--- unity/image_generation/Assets/TakeCameraPicture.cs
+++ unity/image_generation/Assets/TakeCameraPicture.cs
@@ -1,40 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using System.IO;
 
 public class TakeCameraPicture : MonoBehaviour
 {
     public GameObject prefab;
     public GameObject camera;
-    public float turnAmount = 10.0f;
-    public int imgNumber = 0;
-    public int frameNumber = 0;
 
     private GameObject gameObject = null;
 
+    private float x = 0.0f;
+    private float y = 0.0f;
+    private float z = 0.0f;
+    private float angleMax = 360.0f;
+    private float turnAmount = 30.0f;
+    private string objectDesc = "unknown";
+    private int imageCounter = -1;
+    private int maxImages = 1024;
+
+    private bool doInitialCircleAround = true;
+    private int numCircleAround = 64;
+    private bool doTakePictures = true;
+
     void Start()
     {
-        Screen.SetResolution(200, 200, false);
         gameObject = GameObject.Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.Euler(45.0f, 60.0f, 45.0f));
+
+        objectDesc = prefab.name;        
+        if (doTakePictures) {
+            string folderpath = "generated_images/" + objectDesc;
+
+            if (!Directory.Exists(folderpath)) {
+                Directory.CreateDirectory(folderpath);
+            }
+        }
     }
-    void FixedUpdate()
+
+    void Update()
     {
-        Screen.SetResolution(200, 200, false);
-        
-        if (gameObject != null) {
-            float scaleX = Random.Range(0.0f, 2.0f);
-            float scaleY = Random.Range(0.0f, 2.0f);
-            float scaleZ = Random.Range(0.0f, 8.0f);
+        if (doInitialCircleAround) {
+            y = y + (360.0f / (float)numCircleAround);
 
-            gameObject.transform.Rotate(scaleX*turnAmount, scaleY*turnAmount, scaleZ*turnAmount, Space.Self);
-
-            if ((imgNumber < 16) && (frameNumber % 50 == 0)) {
-                string filename = "cube_" + imgNumber.ToString() + ".png";
-                ScreenCapture.CaptureScreenshot(filename);
-                imgNumber++;
+            if (y >= 360.0f) {
+                y -= 360.0f;
             }
 
-            frameNumber++;
+            if (imageCounter >= numCircleAround) {
+                doInitialCircleAround = false;
+            }
+        }
+
+        if (!doInitialCircleAround) {
+            x = Random.Range(0.0f, angleMax);
+            y = Random.Range(0.0f, angleMax);
+            z = Random.Range(0.0f, angleMax);
+        }
+
+        gameObject.transform.eulerAngles = new Vector3(x, y, z);
+
+        if (doTakePictures) {
+            if ((x >= 0.0f) && (y >= 0.0f) && (z >= 0.0f) && (x <= angleMax) && (y <= angleMax) && (z <= angleMax)) {
+                string filename = "generated_images/" + objectDesc + "/" + objectDesc + "_" + imageCounter.ToString("D5") + "_X" + ((int)x).ToString("D3") + "_Y" + ((int)y).ToString("D3") + "_Z" + ((int)z).ToString("D3") + ".png";
+                
+                if (!File.Exists(filename)) {
+                    ScreenCapture.CaptureScreenshot(filename);
+                    imageCounter++;
+                }
+            }
+        }
+
+        if (imageCounter >= maxImages) {
+            doTakePictures = false;
+            EditorApplication.isPlaying = false;
         }
     }
 }
